27d86ea4372c867c79d015bf4e60cfe4c0ee4891
Manuel Martin <manuel.martin@gmail.com>
2020-01-02 11:30:33 +0100

    feat(scoped-elements): add scoped elements feature

---
+++ packages/scoped-elements/src/scoped-html.js
@@ -0,0 +1,21 @@
+/* eslint-disable prefer-spread */
+import { html } from 'lit-html';
+import { transform } from './transform.js';
+import { registerElements } from './scoped-elements.js';
+
+export const createScopedHtml = tags => {
+  const definedTags = registerElements(tags);
+
+  // eslint-disable-next-line func-names
+  return {
+    html() {
+      // eslint-disable-next-line prefer-rest-params
+      const args = Array.apply(null, arguments);
+      const strings = args[0];
+      const values = args.slice(1);
+
+      return html.apply(null, [].concat([transform(strings, definedTags)], values));
+    },
+    tags: definedTags,
+  };
+};
